8375c07befcd92b0043ea5b756171e5a292009d9
davidgabriel23 <daviid.gabriel25@gmail.com>
2023-12-27 22:53:07 -0300

    alert/promp/console.log

---
+++ app.js
@@ -0,0 +1,7 @@
+alert('Boas vindas ao jogo do numero secreto');
+let numeroSecreto = 29;
+let chute = prompt('Escolha um numero entre 1 e 30')
+
+if(numeroSecreto == chute) {
+    console.log('Isso ai você descobriu o numero secreto (29)');
+}
