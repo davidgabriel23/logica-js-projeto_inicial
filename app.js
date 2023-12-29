bf76d39af1f0a2546d62dd597934cc67a81fb276
davidgabriel23 <daviid.gabriel25@gmail.com>
2023-12-28 23:51:06 -0300

    else

--- app.js
+++ app.js
@@ -3,5 +3,50 @@ let numeroSecreto = 29;
 let chute = prompt('Escolha um numero entre 1 e 30')
 
 if(numeroSecreto == chute) {
-    console.log('Isso ai você descobriu o numero secreto (29)');
+    alert(`Isso ai você descobriu o numero secreto ${numeroSecreto}`);
+} else {
+    alert('você errou :(')
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// alert('Boas vidas ao nosso site!!!');
+
+// let nome = 'Lua';
+// let idade = 25;
+// let numeroDeVendas = 50;
+// let saldoDisponivel = 1000;
+
+// alert('Erro! Preencha todos os campos.');
+// let mensagemDeErro = 'Erro! Prencha todos os campos';
+// alert(mensagemDeErro)
+
+// nome = prompt('Qual o nome do seu USUARIO');
+
+
+
+// idade = prompt('Qual a sua idade')
+
+// if(idade >= 18 ) {
+//     alert('Você Pode tirar sua habilitação!!');
+// }
+// else(idade < 18) 
+//     alert('você aidan não pode tirar a habilitação!!');
+
+//     if(nome == 'lua'){
+//         alerta('usaraio correto')
+//     }
+    
