5d51d143a588cac7f0380d9fd495ee6d6420c88c
davidgabriel23 <daviid.gabriel25@gmail.com>
2023-12-30 11:38:38 -0300

    desafios de logica

--- app.js
+++ app.js
@@ -1,26 +1,12 @@
-alert('Boas vindas ao jogo do numero secreto');
-let numeroSecreto = 29;
-let chute = prompt('Escolha um numero entre 1 e 30')
+// alert('Boas vindas ao jogo do numero secreto');
+// let numeroSecreto = 29;
+// let chute = prompt('Escolha um numero entre 1 e 30')
 
-if(numeroSecreto == chute) {
-    alert(`Isso ai você descobriu o numero secreto ${numeroSecreto}`);
-} else {
-    alert('você errou :(')
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// if(numeroSecreto == chute) {
+//     alert(`Isso ai você descobriu o numero secreto ${numeroSecreto}`);
+// } else {
+//     alert('você errou :(')
+// }
 
 
 // alert('Boas vidas ao nosso site!!!');
@@ -50,3 +36,64 @@ if(numeroSecreto == chute) {
 //         alerta('usaraio correto')
 //     }
     
+
+
+
+
+// let fimDaSemana = 'Sabado';
+// let resposta = prompt('Qual o dia da semana')
+
+// if(fimDaSemana == resposta){
+//     alert('Bom final de semana')
+// }
+// else{
+//     alert('Boa semana');
+// }
+
+
+// diaDaSemana = prompt('Qual o dia da semana');
+// if (diaDaSemana == 'Sabado'){
+//     alert('Bom fim de semana');
+// } else if(diaDaSemana == 'Domingo'){
+//     alert('Bom fim de semana');
+// } else{
+//     alert('Boa semana')
+// }
+
+
+// numeroPositivo = prompt('Digite seu numero')
+
+// if(numeroPositivo > 0){
+//     alert('Esse numero é POSITIVO')
+// }else {
+//     alert('Esse numero é NEGATIVO')
+// }
+
+
+
+// seusPontos = prompt('Digite seus pontos')
+
+// if(seusPontos >= 100){
+//     alert('Parabéns, você venceu')
+// }else{
+//     alert('Tente novamente para ganhar')
+// }
+
+
+// nomeDeUsuario = prompt('Qual é o seu usuario');
+
+
+// if(nomeDeUsuario == 'David'){
+//     alert('Boas vindas Senhor David')
+// }else{
+//     alert('AVISO um intruso tentou logar')
+// }
+
+
+numero = prompt('Digite seu NUMERO');
+
+if(numero %2 === 0){
+    alert('Esse numero é PAR')
+}else{
+    alert('Esse numero é IMPAR')
+}
